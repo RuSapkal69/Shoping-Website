11da9b9f5c008c0b89ddbe90f791f2e9e93c19bd
Rushikesh Sapkal <rushikeshsapkal930@gmail.com>
2025-01-20 21:04:18 +0530

    footer added

--- src/Components/Subscribe.jsx
+++ src/Components/Subscribe.jsx
@@ -1,3 +1,5 @@
+import Button from "../Header/Button"
+
 const Subscribe = () => {
   return (
     <section className="max-container flex justify-between items-center max-lg:flex-col gap-10" id='contact-us' >
@@ -5,8 +7,11 @@ const Subscribe = () => {
         Sign Up for
         <span className="text-coral-red"> Updates</span> & Newsletter 
       </h3>
-      <div>
-        <input />
+      <div className="lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray  rounded-full">
+        <input type="text" placeholder="subscribe@nike.com" className="input" />
+        <div className="flex max-sm:justify-end items-center max-sm:w-full">
+          <Button label="Sign Up" fullWidth />
+        </div>
       </div>
     </section>
   )
