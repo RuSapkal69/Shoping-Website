f9ad3101ba4ba2d785fefc72cb1e6ede38cecd5f
Rushikesh Sapkal <rushikeshsapkal930@gmail.com>
2025-01-20 20:29:31 +0530

    new section added 1

--- src/Components/Subscribe.jsx
+++ src/Components/Subscribe.jsx
@@ -1,8 +1,14 @@
 const Subscribe = () => {
   return (
-    <div>
-      
-    </div>
+    <section className="max-container flex justify-between items-center max-lg:flex-col gap-10" id='contact-us' >
+      <h3 className="text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold ">
+        Sign Up for
+        <span className="text-coral-red"> Updates</span> & Newsletter 
+      </h3>
+      <div>
+        <input />
+      </div>
+    </section>
   )
 }
 
